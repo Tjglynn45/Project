7ed3590cefec08d1434b855612b0231a6c7d88f3
Tjglynn45 <76975163+Tjglynn45@users.noreply.github.com>
2021-02-19 14:44:55 -0500

    Add files via upload

---
+++ Project 1/Data/keys.py
@@ -0,0 +1,36 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "client_id = 'Oo7DNF-TUfNLFFtGvsZ4PA'\n",
+    "\n",
+    "api_key = 'y1LKwTXxWvNcCMebE1DYMpIMdxxxlXTWGU2C3Lhz1gdS2vyCk7P2WhJ7f0f3AIPyO4cDnRMBfpKo6kwbhhSOw_ltCM-md5YbyiOYrBC8SFRm1s-griQmq-UdKZomYHYx'\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
